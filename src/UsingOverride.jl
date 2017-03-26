bd1a186b454c6c0175fa3003ec7b23de6f0ce7aa
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2017-03-26 15:56:10 +0200

    initial commit

--- src/UsingOverride.jl
+++ src/UsingOverride.jl
@@ -1,5 +1,23 @@
 module UsingOverride
 
-# package code goes here
+    export @override
 
-end # module
+    macro override(m)
+        esc(_override(m))
+    end
+
+    function _override(m)
+        me = eval(Main,m)
+        ns = names(me)
+        ex = Expr[]
+        for n in ns
+            n == m && continue
+            ov = Expr(:(=),n, Expr(:call,:getfield,m, QuoteNode(n) ) )
+            check = Expr(:call,:isdefined,:Main,QuoteNode(n))
+            push!(ex,:( $check && $ov ))
+        end
+        use_ex = :( using $m )
+        Expr(:block,use_ex,ex...)
+    end
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,50 @@
-using UsingOverride
 using Base.Test
+using UsingOverride
 
-# write your own tests here
-@test 1 == 2
+data = rand(100)
+
+#load M1 for the first time
+module M1
+
+    export T1, x, do_thing
+
+    immutable T1
+        x::Int
+    end
+    x = T1(1)
+    
+    do_thing(x::Vector) = sum(x.^2)
+    do_thing(x::T1) = 1
+end
+
+@override M1
+
+old_T1 = T1(1)
+
+@assert x == T1(1)
+@assert do_thing(x) == 1
+@assert do_thing(data) == sum(data.^2)
+
+# Make some changs to M1
+module M1
+
+    export T1, x, do_thing
+
+    immutable T1
+        x::Int
+        y::Float64
+    end
+    global const x = T1(2,1.0)
+    
+    do_thing(x::Vector) = sin(x.^2)
+    do_thing(x::T1) = 2
+end
+
+@override M1
+
+@assert x == T1(2,1.0)
+@assert do_thing(x) == 2
+@assert do_thing(data) == sin(data.^2)
+
+@test_throws MethodError do_thing(old_T1)
+
